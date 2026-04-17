d4585d2b6b85a32de6d9d8ac6ce3960a20cf7203
Ramazan <mramazantrn28@gmail.com>
2026-04-17 18:32:08 +0300

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,12 @@
+from .ml_20m import ML20MDataset
+from .AnimeRatings import AnimeRatingsDataset
+
+DATASETS = {
+    ML20MDataset.code(): ML20MDataset,
+    "AnimeRatings": AnimeRatingsDataset
+}
+
+
+def dataset_factory(args):
+    dataset = DATASETS[args.dataset_code]
+    return dataset(args)
